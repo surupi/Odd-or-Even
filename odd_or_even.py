c22500400c0690f7d0f9fd01836be16ef0de82c8
surupi <88026590+surupi@users.noreply.github.com>
2022-11-30 21:54:27 +0530

    Delete odd_or_even.py

--- odd_or_even.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-st.title('Tools for Data Science')
-st.subheader('Let us see if a number is ODD or Even')
-placeholder_number = st.empty()
-n = placeholder_number.number_input('Please enter a number: ')
-if float(n)%2 == 0:
-	st.write('This is an EVEN Number')
-  else:
-    st.write('This is an ODD Number')
